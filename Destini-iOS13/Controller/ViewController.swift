0742e9fccd784608379df39a0be1b9978c2bda14
Brandon Wylie <bdonbits@gmail.com>
2020-06-10 09:47:29 -0600

    Implements MVC framework for displaying stories and choice paths

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        storyBrain.currentStory = 0
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let choice = sender.tag
+        if choice == 1 {
+            storyBrain.currentStory = storyBrain.stories[storyBrain.currentStory].choice1Destination
+            updateUI()
+        } else {
+            storyBrain.currentStory = storyBrain.stories[storyBrain.currentStory].choice2Destination
+            updateUI()
+        }
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.stories[storyBrain.currentStory].title
+        choice1Button.setTitle(storyBrain.stories[storyBrain.currentStory].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[storyBrain.currentStory].choice2, for: .normal)
+    }
 }
 
